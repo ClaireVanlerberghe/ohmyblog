06ac0382832ecfde15083c1b984e548a3bacb315
ClaireVanlerberghe <vanlerberghe24@gmail.com>
2024-02-15 21:04:05 +0100

    "register ok"

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Categorie;
 use App\Entity\Utilisateur;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -19,6 +20,8 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
+
+        //Utilisateur
         $claire = new Utilisateur();
         $claire->setEmail('vanlerberghe24@gmail.com');
         $hash = $this->passwordHasher->hashPassword($claire, 'chocolat');
@@ -26,6 +29,37 @@ class AppFixtures extends Fixture
         $claire->setNom('Vanlerberghe');
         $claire->setPrenom('Claire');
         $manager->persist($claire);
+
+        //Utilisateur
+        $marie = new Utilisateur();
+        $marie->setEmail('marie.e.geneste@gmail.com');
+        $hash = $this->passwordHasher->hashPassword($marie, 'azerty');
+        $marie->setPassword($hash);
+        $marie->setNom('Geneste');
+        $marie->setPrenom('Marie');
+        $manager->persist($marie);
+
+        //Catégorie cuisine française
+        $france = new Categorie();
+        $france->setNom('Cuisine française');
+        $manager->persist($france);
+
+        //Catégorie cuisine française
+        $asie = new Categorie();
+        $asie->setNom('Cuisine asiatique');
+        $manager->persist($asie);   
+
+        //Catégorie cuisine française
+        $revisite = new Categorie();
+        $revisite->setNom('Cuisine revisitée');
+        $manager->persist($revisite);
+        
+        //Catégorie cuisine française
+        $dessert = new Categorie();
+        $dessert->setNom('Dessert');
+        $manager->persist($dessert);
+
+
 
         $manager->flush();
     }
